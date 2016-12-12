245054d3e6dae3e662205fef8bd5cc04bd997248
Alessandro Di Maria <adm@m42.ch>
2016-12-12 22:49:19 +0100

    new connection

--- client/app/initialize.js
+++ client/app/initialize.js
@@ -1,4 +1,7 @@
+import Connection from 'lib/connection';
+
 document.addEventListener('DOMContentLoaded', () => {
   // do your setup here
   console.log('Initialized app');
+  new Connection();
 });
--- web_server.rb
+++ web_server.rb
@@ -14,18 +14,40 @@ class WebServer < Reel::Server::HTTP
   end
 
   def route request
-    if request.url == "/"
-      return render request, 'index.html'
-    elsif view_exist? request.path
-      return render request, request.path
+    if request.websocket?
+      route_websocket request
+    else
+      route_request request
     end
+  end
 
-    info "404 Not Found: #{request.path}"
-    request.respond :not_found, "Not found"
+  def route_request request
+    if request.url == "/"
+      render request, 'index.html'
+    elsif request.path == '/socket.io/'
+      render request, '../node_modules/socket.io-client/dist/socket.io.min.js'
+    elsif view_exist? request.path
+      render request, request.path
+    else
+      render_not_found request
+    end
+  end
+
+  def route_websocket request
+    info "Client made a WebSocket request to: #{request.url}"
+    websocket = request.websocket
+
+    websocket << "Hello everyone out there in WebSocket land"
+    websocket.close
   end
 
   def render request, file
     request.respond :ok, view(file)
+  end
+
+  def render_not_found request
+    info "404 Not Found: #{request.path}"
+    request.respond :not_found, "Not found"
   end
 
   def view_exist? file
